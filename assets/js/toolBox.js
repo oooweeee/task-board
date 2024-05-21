5b510320e9cbd9bf2b48c0930807d5faca03df9d
walker <spadonival7@gmail.com>
2024-05-21 19:25:44 -0400

    Removed the chars from the strings for key generation also generated a key based on the js date obj passed, also created a key string with special characters and used a for each loop with an inline function declaration to remove characters from the key

--- assets/js/toolBox.js
+++ assets/js/toolBox.js
@@ -1,2 +1,16 @@
 // strings we want to reuse for console logging 
 const logStrings = {storeErr: "storage Error: " }
+// this will remove the chars from strings for key generation 
+const badKeyChars = ['-', ':', '.', '\\', '[', ']', '{', '}'];
+// this will generate a key based on the js Date obj passed
+function keyGenerator(date){
+    // this will create a key string with the special characters everytime because it is based on time
+    let key = date.toISOString();
+    // use a for loop with an inline function declaration to remove unwanted characters from the key. this is a foreach displayed as a for loop 
+    //for (let i = 0; i < badkeyChars.length; i++) { key = key.replaceAll(badchars[i], ''); }
+    badKeyChars.forEach((char) =>{
+         key = key.replaceAll(char, '');
+    });
+    return key;
+    }
+    
