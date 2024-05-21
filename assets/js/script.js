7155551ce283ad25b49c350bb934c34397462de2
walker <spadonival7@gmail.com>
2024-05-21 11:00:48 -0400

    created a function to generate task id and a fuction to create a task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,37 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
+Date
+
+//  make a button clickable 
+// create a modal to fill out for 3 categoies
+// make add task button clickable for submitting modal after filling it out 
+// needs to go in the column how does it go in to the column
+// create an html element and then append it to the page
+// create dragable function or call
+// now make it deletable
+
+
+const tStatus = {
+    todo: 'to-do',
+    prog: 'in-progress',
+    done: 'done',
+};
+
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    return keyGenerator(new Date());
 
 }
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
 
+// Todo: create a function to create a task card
+function createTaskCard(task) { 
+    let cardData = {
+        class: '',
+        id: task.id,
+        status: task.status,
 }
 
 // Todo: create a function to render the task list and make cards draggable
